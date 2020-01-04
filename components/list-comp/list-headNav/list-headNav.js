8355307de2f1e64b2e7c6bdedd95f5350fb4e19a
xu li <447629057@qq.com>
2020-01-04 21:36:02 +0800

    0104

---
+++ components/list-comp/list-headNav/list-headNav.js
@@ -0,0 +1,23 @@
+// components/list-comp/list-compchildren/list-headNav/list-headNav.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
